a30ff7daf5a7caf1109bb26c240509dcfa3edbf8
Rushikesh-Ambilwade <ambilwaderushikesh58@gmail.com>
2025-04-25 22:53:22 +0530

    add new file

---
+++ app.js
@@ -0,0 +1,24 @@
+let id=document.querySelector(".project");
+id.addEventListener("click",()=>{
+    window.location.href="myproject.html";
+})
+let home=document.querySelector(".Home");
+home.addEventListener("click",()=>{
+    document.location.href="home.html";
+});
+let about=
+document.querySelector(".about");
+about.addEventListener("click",()=>{
+    document.location.href="aboutme.html";
+});
+let con=
+document.querySelector(".con");
+con.addEventListener("click",()=>{
+    document.location.href="contact.html";
+});
+
+let btn=
+document.querySelector(".btn");
+btn.addEventListener("click",()=>{
+    document.location.href="aboutme.html";
+});
